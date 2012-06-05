b3014ba00bef223a87163967459d01e421b442e4
Al B <al@hbo.home>
2012-06-05 14:04:58 +0100

    Installed FOSUserBundle and MopaBootstrapBundle

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new TenK\UserBundle\TenKUserBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new FOS\RestBundle\FOSRestBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
